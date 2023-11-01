949aafc7c496f0b84ccbfa69ccd9255af5dfc5e4
KESHAV <keshavkumar@KESHAVs-MacBook-Air.local>
2023-11-01 20:30:57 +0530

    fontend , backend , react setup done

---
+++ spotify_backend/index.js
@@ -0,0 +1,19 @@
+// npm init  : package.json -- This is a node project
+// npm i express : expressJs package installed
+
+const express = require("express");
+const app = express();
+const port = 4000;
+
+// API GET type "/"
+app.get("/", (req,res) => {
+    //req - contains data for requests
+    //res - contains data for response
+    res.send("Hello World");
+});
+
+// Telling APP to run on specific port number
+
+app.listen(port, () => {
+    console.log("App is running on port : " + port);
+});
