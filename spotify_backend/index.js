b1d7d66f3ed859b3f6594283439412829300ec0d
KESHAV <keshavkumar@KESHAVs-MacBook-Air.local>
2023-11-03 21:00:09 +0530

    playlist routes defined and called

--- spotify_backend/index.js
+++ spotify_backend/index.js
@@ -7,6 +7,7 @@ const passport = require("passport");
 const User = require("./models/Users");
 const authRoutes = require("./routes/auth");
 const songRoutes = require("./routes/song");
+const playlistRoutes = require("./routes/playlist");
 const JwtStrategy = require("passport-jwt").Strategy,
   ExtractJwt = require("passport-jwt").ExtractJwt;
 require("dotenv").config();
@@ -62,6 +63,7 @@ app.get("/", (req, res) => {
 
 app.use("/auth", authRoutes);
 app.use("/song", songRoutes);
+app.use("/playlist", playlistRoutes);
 
 // Telling APP to run on specific port number
 
