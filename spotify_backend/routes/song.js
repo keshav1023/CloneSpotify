b1733f05e9ec5627cee10b96b2cc656eed5af47d
KESHAV <keshavkumar@KESHAVs-MacBook-Air.local>
2023-11-03 19:39:29 +0530

    get routes for finding song by artist name and just by song name added

--- spotify_backend/routes/song.js
+++ spotify_backend/routes/song.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const Song = require("../models/Songs");
+const User = require("../models/Users");
 
-router.post("/create",
+router.post(
+  "/create",
   passport.authenticate("jwt", { session: false }),
   async (req, res) => {
     const { name, thumbnail, track } = req.body;
@@ -28,10 +30,41 @@ router.get(
     const currentUser = req.user;
     // We need to get all songs where artist id == currentUser._id
     const songs = await Song.find({ artist: req.user._id });
-    console.log("Here are my songs : \n",songs);
+    console.log("Here are my songs : \n", songs);
     return res.status(200).json({ data: songs });
   }
-  
+);
+
+// Get route to get all songs any artist has published
+// I will send the artist id and I want to see all songs that artist has published.
+router.get(
+  "/get/artist",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { artistId } = req.body;
+    // We can check if the artist does not exist
+    const artist = await User.find({ _id: artistId });
+    if (!artist) {
+      res.status(301).json({ err: "Artist does not exist !!!" });
+    }
+
+    const songs = await Song.find({ artist: artistId });
+    console.log("Here are your atist songs : \n", songs);
+    return res.status(200).json({ data: songs });
+  }
+);
+
+// Get route to get a single song by name
+router.get(
+  "/get/songname",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { songName } = req.body;
+
+    const songs = await Song.find({ name: songName });
+    console.log("Here are your atist songs : \n", songs);
+    return res.status(200).json({ data: songs });
+  }
 );
 
 module.exports = router;
