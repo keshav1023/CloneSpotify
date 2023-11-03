adf453199e79fdbee5037ba2bd24c910b8360f84
KESHAV <keshavkumar@KESHAVs-MacBook-Air.local>
2023-11-03 21:19:34 +0530

    route for get all playlist of an artist adde

--- spotify_backend/routes/playlist.js
+++ spotify_backend/routes/playlist.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const passport = require("passport");
 const router = express.Router();
-const Playlist = require("../models/Playlist")
+const Playlist = require("../models/Playlist");
+const User = require("../models/Users");
 
 // Create a playlist
 router.post("/create",
@@ -41,6 +42,25 @@ passport.authenticate("jwt", { session: false }),
   }
 );
 
+// Get all playlist of an artist
+router.get("/get/artist:artistId" ,
+passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const artistId =get.params.artistId;
+    
+    const artist = await findOne({_id: artistId});
+    if(!artist){
+        return res.status(301).json({err: "Invalid artist Id !!!"});
+    }
+
+    const playlists = await Playlist.find({owner: artistId});
+    console.log("Here are all playlists of your artist : \n", playlists);
+    return res.status(200).json({data: playlist});
+  }
+);
+
+
+// Add a song to a playlist
 
 
 module.exports = router;
