9953f8d4c5cfe9bb5e1ca219243b8cf79824dfaf
KESHAV <keshavkumar@KESHAVs-MacBook-Air.local>
2023-11-04 08:19:12 +0530

    demo Route added in Routes under BrowserRouter for testing

--- spotify_frontend/src/App.js
+++ spotify_frontend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HelloComponent />} />
+          <Route path="/hi" element={<div>Hi</div>} />
+          <Route path="/bye" element={<div>Bye</div>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
+
+const HelloComponent=()=>{
+  return <div>This is Hello from component</div>;
+};
 
 export default App;
