f49e8e0e5b87b8c9a6594ff97acd42c3ec377a62
KESHAV <keshavkumar@KESHAVs-MacBook-Air.local>
2023-11-04 21:13:48 +0530

    few css changes done

--- spotify_frontend/src/components/shared/IconText.js
+++ spotify_frontend/src/components/shared/IconText.js
@@ -4,7 +4,7 @@ const IconText = ({iconName,displayText,active}) => {
     return (
         <div className="flex items-center justify-start cursor-pointer">
             <div className="px-5 py-2">
-                <Icon icon={iconName} color={active ? "white" : "gray"} fontSize={35} />
+                <Icon icon={iconName} color={active ? "white" : "gray"} fontSize={27} />
             </div>
             <div className={`${active ? "text-white" : "text-gray-400"} text-sm font-semibold hover:text-white`}>{displayText}</div>
         </div>
