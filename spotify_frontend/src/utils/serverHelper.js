323586238526071e31ad7148393050b4cb48fc75
KESHAV <keshavkumar@KESHAVs-MacBook-Air.local>
2023-11-05 23:06:09 +0530

    helper function created for unauthenticated post calls to return the formatted response

--- spotify_frontend/src/utils/serverHelper.js
+++ spotify_frontend/src/utils/serverHelper.js
@@ -1,10 +1,9 @@
 import {backendUrl} from "./config"
-
 export const makeUnauthenticatedPOSTResquest = async (route,body) => {
     const response = await fetch(backendUrl+route, {
-        method:"POST",
-        headers: {
-            "Content-Type":"application/json",
+        method :"POST",
+        headers : {
+            "Content-Type" : "application/json",
         },
         body:JSON.stringify(body),
     });
