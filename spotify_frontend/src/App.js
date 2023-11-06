5ce966fca17c168e6b724d8a9149634e6cdade1b
KESHAV <keshavkumar@KESHAVs-MacBook-Air.local>
2023-11-06 20:26:44 +0530

    upload song route added

--- spotify_frontend/src/App.js
+++ spotify_frontend/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginComponent from "./routes/Login";
 import SignupComponent from "./routes/Signup";
 import HomeComponent from "./routes/Home";
+import LoggedInHomeComponent from "./routes/LoggendInHome"
+import UploadSong from "./routes/UploadSong";
 import { useCookies } from "react-cookie";
 
 function App() {
@@ -14,12 +16,16 @@ function App() {
     <div className="w-screen h-screen font-poppins">
       <BrowserRouter>
         {cookie.token ? (
+          // Logged in routes
           <Routes>
             <Route path="/" element={<HelloComponent />} />
-            <Route path="/home" element={<HomeComponent />} />
+            <Route path="/home" element={<LoggedInHomeComponent />} />
+            <Route path="/uploadSong" element={<UploadSong />} />
             <Route path="*" element={<Navigate to="/home" />} />
           </Routes>
         ) : (
+          
+          // Logged out routes
           <Routes>
             <Route path="/home" element={<HomeComponent />} />
             <Route path="/login" element={<LoginComponent />} />
