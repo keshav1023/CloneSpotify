5960c72fd5b4052fe9ef161d6d7388236588cf74
KESHAV <keshavkumar@KESHAVs-MacBook-Air.local>
2023-11-06 10:10:54 +0530

    restrictring logged in users to go to login and signup page instead redirecting them to /home

--- spotify_frontend/src/App.js
+++ spotify_frontend/src/App.js
@@ -1,28 +1,38 @@
 //import "./App.css";
 import "./output.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginComponent from "./routes/Login";
 import SignupComponent from "./routes/Signup";
 import HomeComponent from "./routes/Home";
-
+import { useCookies } from "react-cookie";
 
 function App() {
+  const [cookie, setCookie] = useCookies(["token"]);
+  console.log(cookie.token);
+
   return (
     <div className="w-screen h-screen font-poppins">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HelloComponent />} />
-          <Route path="/login" element={<LoginComponent />} />
-          <Route path="/signup" element={<SignupComponent />} />
-          <Route path="/home" element={<HomeComponent />} />
-          
-        </Routes>
+        {cookie.token ? (
+          <Routes>
+            <Route path="/" element={<HelloComponent />} />
+            <Route path="/home" element={<HomeComponent />} />
+            <Route path="*" element={<Navigate to="/home" />} />
+          </Routes>
+        ) : (
+          <Routes>
+            <Route path="/home" element={<HomeComponent />} />
+            <Route path="/login" element={<LoginComponent />} />
+            <Route path="/signup" element={<SignupComponent />} />
+            <Route path="*" element={<Navigate to="/login" />} />
+          </Routes>
+        )}
       </BrowserRouter>
     </div>
   );
 }
 
-const HelloComponent=()=>{
+const HelloComponent = () => {
   return <div>This is Hello from component</div>;
 };
 
