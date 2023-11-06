bf7a9defe9f8e3e2d3232d5b442506d104e6acb6
KESHAV <keshavkumar@KESHAVs-MacBook-Air.local>
2023-11-06 12:49:37 +0530

    logging in of a registered user with a cookie as token for next 30 days done

--- spotify_frontend/src/routes/Login.js
+++ spotify_frontend/src/routes/Login.js
@@ -1,9 +1,38 @@
 import { Icon } from "@iconify/react";
+import { useCookies } from "react-cookie";
+import { Link, useNavigate } from "react-router-dom";
 import PasswordInput from "../components/shared/PasswordInput";
 import TextInput from "../components/shared/TextInput";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { makeUnauthenticatedPOSTResquest } from "../utils/serverHelper";
 
 const LoginComponent = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cookie, setCookie] = useCookies(["token"]);
+  const navigate = useNavigate();
+
+  const logIn = async () => {
+    const data = { email, password };
+    const response = await makeUnauthenticatedPOSTResquest(
+      "/auth/login",
+      data
+    );
+    console.log(response);
+    console.log("Response is : "+response);
+    console.log("Response Token is : "+response);
+    if (response && !response.err) {
+      const token = response.token;
+      const date = new Date();
+      date.setDate(date.getDate() + 30);
+      setCookie("token", token, { path: "/", expires: date });
+      alert("Success !!!!");
+      navigate("/home");
+    } else {
+      alert("Failure :( ");
+    }
+  };
+
   return (
     <div className="w-full h-full flex flex-col items-center">
       <div className="logo p-5 border-b border-solid border-grey-400 w-full flex justify-center">
@@ -16,10 +45,23 @@ const LoginComponent = () => {
           label="Email address or username"
           placeholder="Email address or username"
           className="my-6"
+          value={email}
+          setValue={setEmail}
         />
-        <PasswordInput label="Password" placeholder="Password" />
+        <PasswordInput
+          label="Password"
+          placeholder="Password"
+          value={password}
+          setValue={setPassword}
+        />
         <div className="w-full flex items-center justify-end my-8">
-          <button className="bg-green-300 font-semibold p-3 px-10 rounded-full">
+          <button
+            className="bg-green-300 font-semibold p-3 px-10 rounded-full"
+            onClick={(e) => {
+              e.preventDefault();
+              logIn();
+            }}
+          >
             LOG IN
           </button>
         </div>
