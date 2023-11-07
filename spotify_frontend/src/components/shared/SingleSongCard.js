b80de880806d422a3501f3af3b8b5f1d6d7c3821
KESHAV <keshavkumar@KESHAVs-MacBook-Air.local>
2023-11-07 14:11:07 +0530

    artist name rendered in song card

--- spotify_frontend/src/components/shared/SingleSongCard.js
+++ spotify_frontend/src/components/shared/SingleSongCard.js
@@ -1,15 +1,19 @@
-const SingleSongCard = () => {
+const SingleSongCard = ({info}) => {
     return (
         <div className="flex hover:bg-gray-400 hover:bg-opacity-20 p-2 rounded-sm">
             <div className="w-12 h-12 bg-cover bg-center"
                 style={{
-                    backgroundImage: `url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSgO3eEz3PsKnktBWVpsXiyIOCIgZgTudPGg&usqp=CAU)`
+                    backgroundImage: `url("${info.thumbnail}")`
                 }}
             ></div>
             <div className="flex w-full">
                 <div className="text-white flex justify-center flex-col pl-4 w-5/6">
-                    <div className="cursor-pointer hover:underline">Takda hi Jawan</div>
-                    <div className="text-xs text-gray-400 cursor-pointer hover:underline">Prophe C</div>
+                    <div className="cursor-pointer hover:underline">
+                        {info.name}
+                    </div>
+                    <div className="text-xs text-gray-400 cursor-pointer hover:underline">
+                        {info.artist.firstName + " " + info.artist.lastName}
+                    </div>
                 </div>
                 <div className="w-1/6 text-gray-400 text-sm flex justify-center items-center">
                     <div>3.44</div>
