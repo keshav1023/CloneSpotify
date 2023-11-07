5b06234598492f65125c9066f9dad7fe813e5b24
KESHAV <keshavkumar@KESHAVs-MacBook-Air.local>
2023-11-07 10:47:48 +0530

    authenticated POST request method created

--- spotify_frontend/src/utils/serverHelper.js
+++ spotify_frontend/src/utils/serverHelper.js
@@ -10,3 +10,25 @@ export const makeUnauthenticatedPOSTResquest = async (route,body) => {
     const formattedResponse = await response.json();
     return formattedResponse;
 };
+
+export const makeAuthenticatedPOSTResquest = async (route,body) => {
+    const token= getToken();
+    const response = await fetch(backendUrl+route, {
+        method :"POST",
+        headers : {
+            "Content-Type" : "application/json",
+            "Authorization": `Bearer ${token}`,
+        },
+        body:JSON.stringify(body),
+    });
+    const formattedResponse = await response.json();
+    return formattedResponse;
+};
+
+const getToken = () => {
+    const accessToken = document.cookie.replace(
+        /(?:(?:^|.*;\s*)token\s*=\s*([^;]*).*$)|^.*$/,
+        "$1"
+    );
+    return accessToken;
+};
