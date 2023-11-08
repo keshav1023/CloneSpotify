f99e0dac082cba9fd5611e820011c033c85052fc
KESHAV <keshavkumar@KESHAVs-MacBook-Air.local>
2023-11-08 10:28:22 +0530

    logged in container created with left-panel,navbar and song playing bar to be used as component and put content inside it

---
+++ spotify_frontend/src/containers/LoggedInContainer.js
@@ -0,0 +1,160 @@
+import { useState } from "react";
+import { Howl, Howler } from "howler";
+import spotify_logo from "../assets/images/spotify_logo_white.svg";
+import IconText from "../components/shared/IconText";
+import { Icon } from "@iconify/react";
+import TextWithHover from "../components/shared/TextWithHover";
+
+const LoggedInContainer = ({children}) => {
+    const [soundPlayed, setSoundPlayed] = useState(null);
+    const [isPaused, setIsPaused] = useState(true);
+
+    const playSound = (songSrc) => {
+        if (soundPlayed) {
+          soundPlayed.stop();
+        }
+        let sound = new Howl({
+          src: [songSrc],
+          html5: true,
+        });
+        setSoundPlayed(sound);
+        sound.play();
+      };
+
+      const pausedSound = ()=>{
+        soundPlayed.pause();
+      };
+
+      const togglePlayPause = () =>{
+        if(isPaused){
+            playSound("https://res.cloudinary.com/dcrerbv5m/video/upload/v1699332019/bir0uy0kcfxzvnslsork.mp3");
+            setIsPaused(false);
+        }
+        else{
+            pausedSound();
+            setIsPaused(true);
+        }
+      };
+
+  return (
+    <div className="h-full w-full bg-app-black">
+    <div className="h-9/10 w-full flex">
+        {/* Left Panel div */}
+        <div className="h-full w-1/5 bg-black flex flex-col justify-between pb-10">
+            {/* This div is for logo */}
+            <div>
+            <div className="logoDiv p-6">
+                <img src={spotify_logo} alt="Spotify Logo" width={125} />
+            </div>
+            <div className="py-5">
+                <IconText iconName={"material-symbols:home"} displayText={"Home"} />
+                <IconText iconName={"ion:search"} displayText={"Search"} />
+                <IconText
+                iconName={"icomoon-free:books"}
+                displayText={"Your Library"}
+                />
+                <IconText
+                iconName={"material-symbols:library-music-sharp"}
+                displayText={"My music"}
+                />
+            </div>
+            <div className="pt-5">
+                <IconText
+                iconName={"material-symbols:add-box"}
+                displayText={"Create Playlist"}
+                />
+                <IconText
+                iconName={"fluent-emoji:heart-decoration"}
+                displayText={"Liked Songs"}
+                />
+            </div>
+            </div>
+            <div className="px-5">
+            <div className="border border-gray-100 text-white rounded-full w-2/5 flex  px-2 py-1 items-center justify-center hover:border-white cursor-pointer">
+                <Icon icon="humbleicons:globe" />
+                <div className="ml-2 text-sm font-semibold">English</div>
+            </div>
+            </div>
+        </div>
+        {/* Right Panel div */}
+        <div className="h-full w-4/5 bg-app-black overflow-auto">
+            <div className="navbar bg-black bg-opacity-30 w-full h-1/10 flex items-center justify-end">
+                <div className="w-1/2 flex h-full">
+                    <div className="w-2/3 flex justify-around items-center">
+                        <TextWithHover displayText={"Premium"}/>
+                        <TextWithHover displayText={"Support"}/>
+                        <TextWithHover displayText={"Download"}/>
+                        <div className="h-2/3 border-r border-white"></div>
+                    </div>
+                    <div className="w-1/3 flex justify-around h-full items-center">
+                        <TextWithHover displayText={"Upload Songs"}/>
+                        <div className="bg-white w-10 h-10 rounded-full flex items-center justify-center font-semibold cursor-pointer">
+                            AC
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="content p-8 pt-0 overflow-auto">
+                {children}
+            </div>
+        </div>
+      </div>
+      {/* This div is the current playing song */}
+      <div className="w-full h-1/10 bg-black bg-opacity-30 text-white flex items-center px-4">
+       <div className="w-1/4 flex items-center">
+            <img  
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSgO3eEz3PsKnktBWVpsXiyIOCIgZgTudPGg&usqp=CAU" 
+                alt="currentSongThumbnail"
+                className="h-14 w-14 rounded"
+            />
+            <div className="pl-4">
+                <div className="text-sm hover:underline cursor-pointer">Takda hi Jawan</div>
+                <div className="text-xs text-gray-500 hover:underline cursor-pointer">Prophe C</div>
+            </div>
+       </div>
+       <div className="w-1/2 flex justify-center h-full flex-col items-center">
+            <div className="flex w-1/3 justify-between items-center">
+                {/* Controls for playing the song */}
+                <Icon 
+                    icon="ph:shuffle-fill" 
+                    fontSize={30} 
+                    className="cursor-pointer text-gray-500 hover:text-white"
+                    
+                />
+                <Icon 
+                    icon="mi:previous" 
+                    fontSize={30} 
+                    className="cursor-pointer text-gray-500 hover:text-white"
+                    
+                 />
+                <Icon 
+                    icon={isPaused ? "gridicons:play" : "gridicons:pause"} 
+                    fontSize={50} 
+                    className="cursor-pointer text-gray-500 hover:text-white"
+                    onClick={togglePlayPause}
+                 />
+                <Icon 
+                    icon="mi:next" 
+                    fontSize={30} 
+                    className="cursor-pointer text-gray-500 hover:text-white"
+                    
+                 />
+                <Icon 
+                    icon="tabler:repeat" 
+                    fontSize={30} 
+                    className="cursor-pointer text-gray-500 hover:text-white"
+                    
+                 />
+            </div>
+            <div>Progress Bar Here</div>
+       </div>
+       <div className="w-1/4 flex justify-end">
+            Volume control
+       </div>
+      </div>
+    </div>
+  );
+};
+
+
+export default LoggedInContainer;
