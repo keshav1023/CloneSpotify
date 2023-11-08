20a5f64eff31bab81a1dc0dc604d383b12d5ee63
KESHAV <keshavkumar@KESHAVs-MacBook-Air.local>
2023-11-08 15:57:53 +0530

    findOne function update for songs to be stored in name of different users

--- spotify_backend/index.js
+++ spotify_backend/index.js
@@ -43,7 +43,7 @@ opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = "secretKey";
 passport.use(
   new JwtStrategy(opts, function (jwt_payload, done) {
-    User.findOne({ id: jwt_payload.sub }, function (err, user) {
+    User.findOne({_id: jwt_payload.identifier }, function (err, user) {
       if (err) {
         return done(err, false);
       }
