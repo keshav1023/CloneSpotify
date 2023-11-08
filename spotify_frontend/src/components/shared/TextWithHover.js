39e9a614bccb4b26c72cfb0f496036fe04f98643
KESHAV <keshavkumar@KESHAVs-MacBook-Air.local>
2023-11-08 15:54:00 +0530

    targetLink added for login signup

--- spotify_frontend/src/components/shared/TextWithHover.js
+++ spotify_frontend/src/components/shared/TextWithHover.js
@@ -1,8 +1,12 @@
-const TextWithHover = ({displayText,active}) => {
+import { Link } from "react-router-dom";
+
+const TextWithHover = ({displayText,active,targetLink}) => {
     return (
-        <div className="flex items-center justify-start cursor-pointer">
-            <div className={`${active ? "text-white" : "text-gray-500"}  font-semibold text-lg hover:text-white `}>{displayText}</div>
-        </div>
+        <Link to={targetLink}>
+            <div className="flex items-center justify-start cursor-pointer">
+                <div className={`${active ? "text-white" : "text-gray-500"}  font-semibold text-lg hover:text-white `}>{displayText}</div>
+            </div>
+        </Link>
     )
 };
 
