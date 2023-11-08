d456f0aa4939b4c0a97e7399fc8456ce594bc9a8
KESHAV <keshavkumar@KESHAVs-MacBook-Air.local>
2023-11-08 10:30:37 +0530

    code reduced by using logged in container

--- spotify_frontend/src/routes/MyMusic.js
+++ spotify_frontend/src/routes/MyMusic.js
@@ -1,29 +1,10 @@
 import { useState, useEffect } from "react";
-import { Howl, Howler } from "howler";
-import { Icon } from "@iconify/react";
-import spotify_logo from "../assets/images/spotify_logo_white.svg";
-import IconText from "../components/shared/IconText";
 import SingleSongCard from "../components/shared/SingleSongCard";
-import TextWithHover from "../components/shared/TextWithHover";
 import { makeAuthenticatedGETResquest } from "../utils/serverHelper";
+import LoggedInContainer from "../containers/LoggedInContainer";
 
 const MyMusic = () => {
   const [songData, setSongData] = useState([]);
-  const [soundPlayed, setSoundPlayed] = useState(null);
-
-  const playSound = (songSrc) => {
-    if (soundPlayed) {
-      soundPlayed.stop();
-    }
-    let sound = new Howl({
-      src: [songSrc],
-      html5: true,
-    });
-    setSoundPlayed(sound);
-    sound.play();
-    console.log(sound);
-  };
-
   useEffect(() => {
     //fetch data
     const getData = async () => {
@@ -34,75 +15,14 @@ const MyMusic = () => {
   }, []);
 
   return (
-    <div className="h-full w-full flex">
-      {/* Left Panel div */}
-      <div className="h-full w-1/5 bg-black flex flex-col justify-between pb-10">
-        {/* This div is for logo */}
-        <div>
-          <div className="logoDiv p-6">
-            <img src={spotify_logo} alt="Spotify Logo" width={125} />
-          </div>
-          <div className="py-5">
-            <IconText iconName={"material-symbols:home"} displayText={"Home"} />
-            <IconText iconName={"ion:search"} displayText={"Search"} />
-            <IconText
-              iconName={"icomoon-free:books"}
-              displayText={"Your Library"}
-            />
-            <IconText
-              iconName={"material-symbols:library-music-sharp"}
-              displayText={"My music"}
-              active
-            />
-          </div>
-          <div className="pt-5">
-            <IconText
-              iconName={"material-symbols:add-box"}
-              displayText={"Create Playlist"}
-            />
-            <IconText
-              iconName={"fluent-emoji:heart-decoration"}
-              displayText={"Liked Songs"}
-            />
-          </div>
-        </div>
-        <div className="px-5">
-          <div className="border border-gray-100 text-white rounded-full w-2/5 flex  px-2 py-1 items-center justify-center hover:border-white cursor-pointer">
-            <Icon icon="humbleicons:globe" />
-            <div className="ml-2 text-sm font-semibold">English</div>
-          </div>
-        </div>
+    <LoggedInContainer>
+      <div className="text-white text-xl font-semibold pb-4 pl-2 pt-8">My Songs</div>
+      <div className="space-y-3 overflow-auto">
+        {songData.map((item) => {
+          return <SingleSongCard info={item} playSound={() => {}} />;
+        })}
       </div>
-      {/* Right Panel div */}
-      <div className="h-full w-4/5 bg-app-black overflow-auto">
-        <div className="navbar bg-black bg-opacity-30 w-full h-1/10 flex items-center justify-end">
-          <div className="w-1/2 flex h-full">
-            <div className="w-3/5 flex justify-around items-center">
-              <TextWithHover displayText={"Premium"} />
-              <TextWithHover displayText={"Support"} />
-              <TextWithHover displayText={"Download"} />
-              <div className="h-2/3 border-r border-white"></div>
-            </div>
-            <div className="w-1/3 flex justify-around h-full items-center">
-              <TextWithHover displayText={"Upload Songs"} />
-              <div className="bg-white w-10 h-10 rounded-full flex items-center justify-center font-semibold cursor-pointer">
-                AC
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="content p-8 overflow-auto">
-          <div className="text-white text-xl font-semibold pb-4 pl-2">
-            My Songs
-          </div>
-          <div className="space-y-3 overflow-auto">
-            {songData.map((item) => {
-              return <SingleSongCard info={item} playSound={playSound} />;
-            })}
-          </div>
-        </div>
-      </div>
-    </div>
+    </LoggedInContainer>
   );
 };
 
