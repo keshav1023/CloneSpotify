f7fb23537480f55c8bb5417babc7b26df1327b4e
KESHAV <keshavkumar@KESHAVs-MacBook-Air.local>
2023-11-09 17:59:56 +0530

    artist details populated in songs

--- spotify_backend/routes/playlist.js
+++ spotify_backend/routes/playlist.js
@@ -35,7 +35,12 @@ router.get("/get/playlist/:playlistId" ,
 passport.authenticate("jwt", { session: false }),
   async (req, res) => {
     const playlistId = req.params.playlistId;
-    const playlist = await Playlist.findOne({_id: playlistId});
+    const playlist = await Playlist.findOne({_id: playlistId}).populate({
+      path: "songs",
+      populate: {
+        path: "artist",
+      },
+    });
     if(!playlist){
         return res.status(301).json({err: "Invalid Id !!!"});
     }
